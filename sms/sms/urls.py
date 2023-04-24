863796b35cd48bbf935b81517a87185b9697f69b
fujingnan <fujingnan@xueqiu.com>
2023-04-24 22:59:13 +0800

    create database and download boostrap&jquery

--- sms/sms/urls.py
+++ sms/sms/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..manageApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('depart/list/', views.depart_list)
 ]
