d51ad089cac07102fb2f12db056abca355f0c3c6
fujingnan <291217208@qq.com>
2023-04-25 15:40:54 +0800

    improve depart_list.html

--- sms/sms/settings.py
+++ sms/sms/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), '../manageApp/static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- sms/sms/urls.py
+++ sms/sms/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..manageApp import views
+from manageApp import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
